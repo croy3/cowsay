1bc023374a780cab23993dc1b132fdd152f4bae7
croy3 <119435478+croy3@users.noreply.github.com>
2023-01-09 12:15:36 +0100

    add files

---
+++ index.js
@@ -0,0 +1,8 @@
+const userInfo = require('./information');
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text: "Hello I'm " + userInfo.name + " from " + userInfo.campus,
+    e: "oO",
+    T: "U"
+}));
---
+++ information.js
@@ -0,0 +1,7 @@
+const name = "Camille Roy";
+const campus = "Remote"; 
+
+module.exports = {
+    name: name,
+    campus: campus,
+}
